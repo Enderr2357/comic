d69d8a8dd80d914a05a30dae8cbd477e8ac88ea1
van <909036273@qq.com>
2023-05-19 10:19:35 +0800

    10：19 0519
    主页修改

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -1,24 +1,54 @@
 <template>
   <div class="bg">
-    <div class="title">欢迎使用漫画网站</div>
+    <div class="titlebig">欢迎使用漫画网站</div><br>
+    <ul>
+      <li><el-link type="info">全部</el-link></li>
+      <li><el-link type="info">国漫</el-link></li>
+      <li><el-link type="info">日漫</el-link></li>
+      <li><el-link type="info">校园</el-link></li>
+      <li><el-link type="info">青年</el-link></li>
+      <li><el-link type="info">玄幻</el-link></li>
+      <li><el-link type="info">恋爱</el-link></li>
+      <li><el-link type="info">武侠</el-link></li>
+      <li><el-link type="info">恐怖</el-link></li>
+      <li><el-link type="info">搞笑</el-link></li>
+      <li><el-link type="info">都市</el-link></li>
+    </ul>
+    <div class="titlemid">热门更新</div>
+    <div>
+      <el-link href="./Detail">这里是漫画</el-link>
     </div>
-  <div>
-    <el-link href="./Detail">这里是漫画</el-link>
   </div>
 </template>
 <script setup>
 </script>
 <style>
-.bg {
-  height: 200px;
-  background-image: url('../img/主页背景图片三.jpg');
-  background-size: 100% 100%;
+ul {
+  text-align: center;
 }
 
-.title {
+li {
+  display: inline;
+  margin-left: 1%;
+}
+
+.bg {
+  height: auto;
+  background-image: url('../img/主页背景图片二.jpg');
+  background-size: 100% 100%;
+  background-attachment: fixed;
+}
+
+.titlebig {
   font-size: xx-large;
   text-align: center;
   color: white;
+}
+
+.titlemid {
+  margin-left: 15%;
+  color: white;
+  font-size: x-large;
 }
 
 .el-main {
