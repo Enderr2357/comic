cd18edce736b2c2c8bdca672975621ee94768fa8
van <909036273@qq.com>
2023-05-19 10:30:08 +0800

    Auto stash before merge of "master" and "origin/master"

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,24 +1,20 @@
 <script setup>
-  const imgUrl= new URL('../img/comicimg/biaorenfm.jpg',import.meta.url).href
+const imgUrl = new URL('../img/comicimg/biaorenfm.jpg', import.meta.url).href
 </script>
 <template>
   <div class="comicInfo">
     <div class="comicImg">
-      <el-image :src="imgUrl"
-        style="width: 100px; height: 100px"
-        :zoom-rate="1.2"
-        :preview-src-list="[imgUrl]"
-        :initial-index="1"
-        fit="cover" 
-        />
+      <el-image :src="imgUrl" style="width: 100px; height: 100px" :zoom-rate="1.2" :preview-src-list="[imgUrl]"
+        :initial-index="1" fit="cover" />
     </div>
   </div>
 </template>
 <style scoped>
-.image-slot{
-   font-size: 30px;
+.image-slot {
+  font-size: 30px;
 }
-.el-image{
+
+.el-image {
   width: 100%;
   height: 200px;
 }
