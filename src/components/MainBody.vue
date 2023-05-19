1782cfe960459f061f1bf37ba58a7f4dbd934843
van <909036273@qq.com>
2023-05-19 12:03:45 +0800

    12:03 0519(W
    主页修改

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -60,13 +60,54 @@
           </div>
         </el-col>
       </el-row>
-    </div>
-
-    <!-- <div>
-      <a href="/Detail">
-        <el-image :src="imgUrl"></el-image>
-      </a>
-    </div> -->
+    </div><br>
+    <div class="titlemid">人气排行</div><br>
+    <div class="main">
+      <el-row gutter="20">
+        <el-col :span="3">
+          <div><a href="/Detail">
+              <el-image :src="imgUrl7"></el-image>
+            </a>
+            <div class="titlesmall">赤地万里</div>
+          </div>
+        </el-col>
+        <el-col :span="3">
+          <div><a href="/Detail">
+              <el-image :src="imgUrl8"></el-image>
+            </a>
+            <div class="titlesmall">回想</div>
+          </div>
+        </el-col>
+        <el-col :span="3">
+          <div><a href="/Detail">
+              <el-image :src="imgUrl9"></el-image>
+            </a>
+            <div class="titlesmall">灵墟游记</div>
+          </div>
+        </el-col>
+        <el-col :span="3">
+          <div><a href="/Detail">
+              <el-image :src="imgUrl10"></el-image>
+            </a>
+            <div class="titlesmall">猫之茗</div>
+          </div>
+        </el-col>
+        <el-col :span="3">
+          <div><a href="/Detail">
+              <el-image :src="imgUrl11"></el-image>
+            </a>
+            <div class="titlesmall">史上最强</div>
+          </div>
+        </el-col>
+        <el-col :span="3">
+          <div><a href="/Detail">
+              <el-image :src="imgUrl12"></el-image>
+            </a>
+            <div class="titlesmall">三月初三</div>
+          </div>
+        </el-col>
+      </el-row>
+    </div><br>
   </div>
 </template>
 <script setup>
@@ -76,12 +117,19 @@ const imgUrl3 = new URL('../img/comicimg/刺客信条：王朝.jpg', import.meta
 const imgUrl4 = new URL('../img/comicimg/凤归.jpg', import.meta.url).href
 const imgUrl5 = new URL('../img/comicimg/诡水疑云.jpg', import.meta.url).href
 const imgUrl6 = new URL('../img/comicimg/国魂.jpg', import.meta.url).href
+const imgUrl7 = new URL('../img/comicimg/赤地万里.jpg', import.meta.url).href
+const imgUrl8 = new URL('../img/comicimg/回想.jpg', import.meta.url).href
+const imgUrl9 = new URL('../img/comicimg/灵墟游记.jpg', import.meta.url).href
+const imgUrl10 = new URL('../img/comicimg/猫之茗.jpg', import.meta.url).href
+const imgUrl11 = new URL('../img/comicimg/史上最强.jpg', import.meta.url).href
+const imgUrl12 = new URL('../img/comicimg/三月初三.jpg', import.meta.url).href
 </script>
 
 <style>
 .el-row {
   margin-right: 0% !important;
 }
+
 
 ul {
   text-align: center;
