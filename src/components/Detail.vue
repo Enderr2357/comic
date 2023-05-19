08c0bac91f8f7ade172abf490674846e19211151
ender2357 <y2594254561@outlook.com>
2023-05-19 10:23:11 +0800

    10:22 0519(Y
    主页合并 详情页简略图

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,13 +1,25 @@
-<script setup>
+<script lang="ts" setup>
   const imgUrl= new URL('../img/comicimg/biaorenfm.jpg', import.meta.url).href
 </script>
 <template>
   <div class="comicInfo">
     <div class="comicImg">
-      <img :src="imgUrl" alt="">
+      <el-image :src="imgUrl"
+        style="width: 100px; height: 100px"
+        :zoom-rate="1.2"
+        :preview-src-list="[imgUrl]"
+        :initial-index="1"
+        fit="cover" 
+        />
     </div>
   </div>
 </template>
 <style scoped>
-
+.image-slot{
+   font-size: 30px;
+}
+.el-image{
+  width: 100%;
+  height: 200px;
+}
 </style>
